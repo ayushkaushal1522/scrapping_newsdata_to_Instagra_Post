216a18ca049d8c9e6855a476c626c3b5d74a68ff
aushkaushal1522 <ayushkaushal1522@gmail.com>
2023-09-11 23:03:37 +0530

    image scrapping

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+@app.route('/',methods=["GET","POST"])
+def index():
+    
+    url = "https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content,"html.parser")
+    outerdata = soup.find_all('div',class_="widget-listing" , limit=6)
+
+    finalnews=""
+    for data in outerdata:
+        news = data.div.div.a["title"]
+        finalnews += "\u2022" + news + "\n"
+
+    return render_template("index.html",News = finalnews)
+ 
+
+# if __name__ == "__main__":
+#     from waitress import serve
+#     serve(app, host="0.0.0.0", port=5000)
+
+
+
